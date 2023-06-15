1de0692524a64e5cf8d8673f76f20993e02c51ce
Sidra Waseem <sidraw24@gmail.com>
2023-06-15 09:31:58 +0500

    final changes

---
+++ backend/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); 
+const port = 5000;
+let mongoUrl="mongodb+srv://mussafarariaz:ecproject@cluster0.unm3ics.mongodb.net/?retryWrites=true&w=majority"
+// Connect to MongoDB
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error(err));
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware
+app.use(express.json());
+
+// Signup route
+app.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Login route
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+    const token = jwt.sign({ userId: user._id }, 'secret_key');
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,7 +2,6 @@ import React from 'react'
 import '../Signup/Signup.css'
 import { Link } from 'react-router-dom';
 function Signup() {
-   
   return (
     <div id="signup-page">
       <svg
