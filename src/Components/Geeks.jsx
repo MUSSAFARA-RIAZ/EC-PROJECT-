8715e9acd7face5c4fe7a02dfc9647f5487b7af2
Sidra Waseem <sidraw24@gmail.com>
2023-06-15 09:40:14 +0500

    update UI

--- src/Components/Geeks.jsx
+++ src/Components/Geeks.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function Geeks() {
+  const blogs = (event) => {
+    event.stopPropagation();
+    window.open('http://localhost:3000/blog',"_blank")
+  }
   return (
     <div className='geekdiv'>
       <h1 className='geekhead'>Geek Out with Us</h1>
@@ -8,6 +13,7 @@ export default function Geeks() {
       <br/>
       <p className='geektext'>Welcome to our blog section, where you'll find a treasure trove of coding tutorials designed to enhance your programming skills. Whether you're a beginner dipping your toes into the vast sea of coding or an experienced developer seeking new insights, we have something for everyone.
       Our blog covers a wide range of programming languages, frameworks, and tools, ensuring there's always something to pique your interest. From Python to JavaScript, from web development to machine learning, our tutorials delve into various aspects of coding to help you level up your proficiency</p>
+      <Button variant="primary" style={{position:"relative",left:100}}onClick={blogs}>View Blogs</Button>
     </div>
   )
 }
